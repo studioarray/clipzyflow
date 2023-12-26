fd81b3d87c7c4482dbaeb8e7928871a013b30e3a
Jens Janson <janson@hey.com>
2023-12-26 15:52:15 +0100

    Fix bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import Logo from '../assets/clipzyflow-logo.svg';
+import Logo from "../assets/clipzyflow-logo.svg";
 
 const Header = function () {
-  return <header>
-    <Logo />
-  </header>;
-}
+  return (
+    <header>
+      <Logo />
+    </header>
+  );
+};
 
 export default Header;
